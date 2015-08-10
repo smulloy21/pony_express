35fee2a9ea5a8e6b02785ef40156191df228fcd1
Shannon Mulloy & Rachael Berecka <student@epicodus.com>
2015-08-10 12:25:31 -0700

    calculate shipping cost based on size, weight, and distances. Pass all specs

--- lib/pony_epress.rb
+++ lib/pony_epress.rb
@@ -8,4 +8,17 @@ class Parcel
   define_method(:volume) do
     @length * @width * @height
   end
+  define_method(:cost_to_ship) do |distance|
+    cost = 0
+    if distance <= 15
+      cost += 1
+    elsif distance <= 60
+      cost += 2
+    else
+      cost += 3
+    end
+    cost += (@weight / 10)
+    cost += (volume() / 100)
+    cost
+  end
 end
--- spec/pony_epress_spec.rb
+++ spec/pony_epress_spec.rb
@@ -8,4 +8,14 @@ describe(Parcel) do
       expect(test_parcel.volume()).to(eq(48))
     end
   end
+  describe('#cost_to_ship') do
+    it('calculates shipping cost based on size, weight and distance') do
+      test_parcel = Parcel.new(4, 6, 10, 10)
+      expect(test_parcel.cost_to_ship(50)).to(eq(5))
+    end
+    it('calculates shipping cost based on size, weight and distance') do
+      test_parcel = Parcel.new(6, 6, 6, 10)
+      expect(test_parcel.cost_to_ship(10)).to(eq(4))
+    end
+  end
 end
